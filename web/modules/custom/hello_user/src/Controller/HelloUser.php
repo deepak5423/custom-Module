4a078dab43c7e7206267903a6ed48f9f3aa576b6
deepak5423 <deepak.pandey@innoraft.com>
2023-07-16 13:20:04 +0530

    FT2023-303: Added the comments.

--- web/modules/custom/hello_user/src/Controller/HelloUser.php
+++ web/modules/custom/hello_user/src/Controller/HelloUser.php
@@ -1,12 +1,22 @@
 <?php
 
+/**
+ * @file
+ * Hello Module's front controller.
+ */
+
 namespace Drupal\hello_user\Controller;
 
-use Drupal;
 use Drupal\Core\Controller\ControllerBase;
 
 class HelloUser extends ControllerBase {
+
+    /**
+     * @method view()
+     *  Fetches current user and displays greet message.
+     */
     public function view() {
+        // Storing the current user username.
         $current_user_name = \Drupal::currentUser()->getAccountName();
         return [
             '#type' => 'markup',
